b28030b76e70ed7dbd34f6563c3b1dd00352e5c0
haruvv <haruki.ito0044@gmail.com>
2025-03-15 07:36:05 +0900

    アイコンの修正

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -25,15 +25,16 @@ export const Navbar = () => {
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
-            strokeWidth="2"
             strokeLinecap="round"
             strokeLinejoin="round"
+            strokeWidth="2"
             className="text-primary"
           >
-            <path d="M8 2c4 0 8 1.7 8 6l-.2.4C18.3 7.8 20 9 20 12c0 1.3-.4 2.3-1 3" />
-            <path d="M16 18c-1.9 1.5-4.3 2-7.5 2A9.1 9.1 0 0 1 4 22" />
-            <path d="M4 14.5c0-1.6.4-2.8 1-3.7" />
-            <path d="M2 12a10 10 0 0 0 16.5-8" />
+            {/* モダンな三角形 */}
+            <path d="M6 8 L18 8 L12 14 Z" />
+
+            {/* 垂直線 */}
+            <line x1="12" y1="14" x2="12" y2="18" />
           </svg>
           Haru Tech Blog
         </Link>
